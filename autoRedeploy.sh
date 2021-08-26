44e14ecada77a63ea8c39b544ac8228f274b63c2
philios33 <phil@code67.com>
2021-08-27 01:32:06 +0200

    Really trigger redeploy

--- autoRedeploy.sh
+++ autoRedeploy.sh
@@ -7,9 +7,7 @@ docker exec -i -t predictor-league_predictor_1 node /home/node/serverDist/redepl
 retVal=$?
 if [ $retVal -eq 5 ]; then
     echo "5 returned, do a redeploy..."
-    #git pull
-    #docker-compose build
-    #docker-compose up -d
+    source redeploy.sh
 else
     echo "$retVal returned, not deploying"
 fi
