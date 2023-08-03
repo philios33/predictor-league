6c824e2ff6908fb86b53238a0b69310c518cb5fb
Philip Nicholls <philip.nicholls@forsman.com>
2023-08-03 17:46:30 +0100

    Bug fix for Safari iOS feature check

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -31,7 +31,7 @@ export default function Notifications() {
         if (!('serviceWorker' in navigator)) {
           throw new Error('No Service Worker support!')
         }
-        if (!('PushManager' in window)) {
+        if (!('Notification' in window)) {
           throw new Error('No Push API Support!')
         }
     }
